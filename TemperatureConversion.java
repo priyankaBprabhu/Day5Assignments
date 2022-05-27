a833b9883177a41c9302c58c25a9c4b8063f4a75
priyanka B P <priyankaprabhu96@gmail.com>
2022-05-27 08:17:52 +0530

    Added code to Temperature Converssion

--- TemperatureConversion.java
+++ TemperatureConversion.java
@@ -3,15 +3,22 @@ package com.bridgelabs.java;
 import java.util.Scanner;
 
 public class TemperatureConversion {
-    static int celsius( int temp ){
+    static float celsius( float temp ){
         return (temp - 32) * 5 / 9;
+    }
+    static float fahrenheit( float temp ){
+        return (temp * 9 / 5 )  + 32;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the  Temperature fahrenheit");
-        int temperature = sc.nextInt();
-        int tempInCelsius = celsius( temperature);
+        float temperatureFahrenheit = sc.nextFloat();
+        float tempInCelsius = celsius( temperatureFahrenheit);
         System.out.println("temperature  is  " + tempInCelsius + " Degrees Celsius");
+        System.out.println("Enter the  Temperature fahrenheit");
+        float temperatureDegree = sc.nextFloat();
+        float tempInFahrenheit = fahrenheit( temperatureDegree);
+        System.out.println("temperature  is  " + tempInFahrenheit + " Fahrenheit");
     }
 
 }
